32f2bee8f9d4b4c13e446edd4f73fb5c857850b6
racase <rafacarm@gmail.com>
2017-04-28 23:42:32 +0200

    Se agrega componente Heroe. Routes con parametros

--- ComicApp/src/app/components/heroes/heroes.component.ts
+++ ComicApp/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +10,15 @@ export class HeroesComponent implements OnInit {
 
   heroes:any[] = [];
 
-  constructor( private _heroesServices: HeroesService) { }
+  constructor( private _heroesServices: HeroesService, private router: Router) { }
 
   ngOnInit() {
     this.heroes = this._heroesServices.getHeroes();
     console.log  (this.heroes);
   }
+
+  verHeroe( idx: number) {
+    this.router.navigate([ '/heroe', idx])
+ }
 
 }
